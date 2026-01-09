39a2e2d80f5e1934ba1387ad4cd915ff000955c1
cactacaea <kristina.krivova@ucas-edu.net>
2026-01-09 10:37:23 -0700

    01.09.26 finance calc

---
+++ individual_projects/finance_calc.py
@@ -0,0 +1,56 @@
+# KK 2nd, Finance Caclulator // Practice
+
+import sys
+import time
+
+# "again?" function
+    # user input to calculate something again
+    # if yes call main
+    # else stop running everything and print "stopping"
+
+# main func
+    # while LOOP
+    # input getting user choice for what to calculate (starting menu) // save for a goal, compound interest, budget allocator, sales, tip
+    # if input == an available option, call the necessary function
+
+# save for a goal func
+    # input what users goal is
+    # input how often user is contributing (weekly/monthly)
+    # input how much user is contributing
+
+    # if weekly contributions:
+        # goal divided by contribution #
+            # print how many weeks
+            # wait a few seconds and ask to calculate again
+    # if monthly contributionsL
+        # goal divided by contribution #
+            # print how many weeks
+            # wait a few seconds and ask to calculate again
+
+
+# compound interest calc function
+
+# budget allocator function
+    # empty categories list
+    categories = []
+    # user input how many budget categories they have
+    # make budget categories into an integer
+    # for loop in the range of the budget category integer
+        # input what type of budget category it is
+
+# sales function
+    # original item cost input
+    # discount percent input
+    # calculated discount = original*discount percent/100
+    # final price = original item - discount
+    # print final price
+    # wait a few seconds and ask to calculate again
+
+
+# tip calc function
+    # user input for the inital bill
+    # user input for tip percent
+    # tip amount = orginal_price*tip/100
+    # total = original_price + tip amount
+    # display results
+    # call again? function
